40f474f5c66791fa34cfaa6d8e2a5477e2102a89
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-08-10 15:08:29 -0700

    added basic models and seed data

--- server/models.py
+++ server/models.py
@@ -3,4 +3,50 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+
+
+class User(db.Model, SerializerMixin):
+
+    __tablename__ = 'users'
+
+    serialize_rules = ('-reports.user',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String(8))
+
+    reports = db.relationship('Report', back_populates='user', cascade='all, delete-orphan')
+
+
+
+class Pet(db.Model, SerializerMixin):
+
+    __tablename__ = 'pets'
+
+    serialize_rules = ('-reports.pet',)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    breed = db.Column(db.String)
+    image_url = db.Column(db.String)
+    description = db.Column(db.String)
+
+    reports = db.relationship('Report', back_populates='pet', cascade='all, delete-orphan')
+
+
+
+class Report(db.Model, SerializerMixin):
+
+    __tablename__ = 'reports'
+
+    serialize_rules = ('-pet.reports', '-user.reports',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    report_type = db.Column(db.String, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pet_id = db.Column(db.Integer, db.ForeignKey('pets.id'))
+
+    user = db.relationship('User', back_populates='reports')
+    pet = db.relationship('Pet', back_populates='reports')
+    
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,61 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from config import db
+from models import User, Pet, Report
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+        print("Generating seed data...")
+        
+        print("Deleting all users...")
+        User.query.delete()
+        print("Deleting all pets...")
+        Pet.query.delete()
+        print("Deleting all reports...")
+        Report.query.delete()
+
+        print("Generating users...")
+        users = []
+        usernames = []
+        for i in range(20):
+            username = fake.first_name()
+            while username in usernames:
+                username = fake.first_name()
+            usernames.append(username)
+            user = User(
+                username=username, 
+                password=fake.word()
+                )
+            users.append(user)
+        db.session.add_all(users)
+        db.session.commit()
+
+        print("Generating pets...")
+        pets = []
+        for i in range(40):
+            pet = Pet(
+                name=fake.name(), 
+                breed=fake.word(), 
+                image_url=fake.url(),
+                description=fake.paragraph(nb_sentences=1)
+                )
+            pets.append(pet)
+        db.session.add_all(pets)
+        db.session.commit()
+
+        print("Generating reports...")
+        reports = []
+        for pet in pets:
+            report = Report(
+                report_type=rc(['found', 'lost', 'found', 'found']),
+                user_id=rc([user.id for user in User.query.all()]),
+                pet_id=pet.id
+            )
+            reports.append(report)
+        db.session.add_all(reports)
+
+        db.session.commit()
+        print("Seed data generated successfully.")
+
