f2012dd9353430072012618fddaace60f6bd7644
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-08-25 23:32:44 -0700

    added NavBar component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import NavBar from "./NavBar";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return <NavBar/>;
 }
 
 export default App;
