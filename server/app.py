94267d1ac88547fd1eea59450f60697026d25108
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-09-01 11:28:52 -0700

    added Signup resource

--- server/app.py
+++ server/app.py
@@ -10,6 +10,21 @@ from flask_restful import Resource
 from config import app, db, api
 from models import User, Pet, Report
 
+json=request.get_json()
+
+class Signup(Resource):
+    def post(self):
+        username = json.get('username')
+        password = json.get('password')
+        if not username:
+            return {'message': 'Username required'}, 422
+        if not password:
+            return {'message': 'Password required'}, 422
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user.to_dict(), 201
+
 
 class Pets(Resource):
 
@@ -22,6 +37,7 @@ def index():
     return '<h1>Project Server</h1>'
 
 api.add_resource(Pets, '/pets', endpoint='pets')
+api.add_resource(Signup, '/signup', endpoint='signup')
 
 
 if __name__ == '__main__':
