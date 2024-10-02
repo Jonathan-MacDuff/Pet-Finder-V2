8e11f6dc8149f95f7ecdef0eaa0e14770e142775
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-10-02 13:31:13 -0700

    added formik validations for signup form and new pet form

--- client/src/pages/NewPetForm.js
+++ client/src/pages/NewPetForm.js
@@ -1,51 +1,79 @@
-import {React, useState} from "react";
+import {React} from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 function NewPetForm() {
 
-    const [name, setName] = useState('')
-    const [breed, setBreed] = useState('')
-    const [image_url, setImage] = useState('')
-    const [description, setDescription] = useState('')
-    const [lost, setLost] = useState(false)
-    const [found, setFound] = useState(false)
+    const formSchema = yup.object().shape({
+        name: yup.string().required('Name is required, enter "N/A" if unknown').max(20),
+        breed: yup.string().required('Breed is required, enter "N/A" if unknown').max(20),
+        image_url: yup.string().required('Image is required, enter "N/A" if unavailable').max(50),
+        description: yup.string().required('Description is required').max(200),
+        lost: yup.boolean(),
+        found: yup.boolean(),
+    }).test('lost-or-found', 'Please select either "Lost" or "Found", but not both', function (values) {
+        if (values.lost == values.found) {
+            return this.createError({path: 'lost-or-found', message: 'Please select either "Lost" or "Found", but not both'})
+        }
+        else return true;
+    });
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        fetch('/petform', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({name, breed, image_url, description, lost, found}),
-        });
-    };
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            breed: '',
+            image_url: '',
+            description: '',
+            lost: false,
+            found: false,
+        },
+        validationSchema: formSchema,
+        onSubmit: (values) => {
+            fetch('/petform', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(values, null, 2),
+            });
+        }
+    });
 
     return (
-        <form onSubmit = {handleSubmit}>
+        <form onSubmit = {formik.handleSubmit}>
             <label>Name
-                <input type='text' id='name' value={name}
-                    onChange={(e) => setName(e.target.value)}/>
+                <input type='text' id='name' name='name' value={formik.values.name}
+                    onChange={formik.handleChange}/>
+                <p style={{color:'red'}}>{formik.errors.name}</p>
             </label>
             <label>Breed
-                <input type='text' id='breed' value={breed}
-                    onChange={(e) => setBreed(e.target.value)}/>
+                <input type='text' id='breed' name='breed' value={formik.values.breed}
+                    onChange={formik.handleChange}/>
+                <p style={{color:'red'}}>{formik.errors.breed}</p>
             </label>
             <label>Image
-                <input type='text' id='image' value={image_url}
-                    onChange={(e) => setImage(e.target.value)}/>
+                <input type='text' id='image' name='image_url' value={formik.values.image_url}
+                    onChange={formik.handleChange}/>
+                <p style={{color:'red'}}>{formik.errors.image_url}</p>
             </label>
             <label>Description
-                <input type='text' id='description' value={description}
-                    onChange={(e) => setDescription(e.target.value)}/>
+                <input type='text' id='description' name='description' value={formik.values.description}
+                    onChange={formik.handleChange}/>
+                <p style={{color:'red'}}>{formik.errors.description}</p>
             </label>
             <label>Lost
-                <input type='checkbox' id='lost' checked={lost}
-                    onChange={(e) => setLost(e.target.checked)}/>
+                <input type='checkbox' id='lost' name='lost' checked={formik.values.lost}
+                    onChange={formik.handleChange}/>
+                <p style={{color:'red'}}>{formik.errors.lost}</p>
             </label>
             <label>Found
-                <input type='checkbox' id='found' checked={found}
-                    onChange={(e) => setFound(e.target.checked)}/>
+                <input type='checkbox' id='found' name='found' checked={formik.values.found}
+                    onChange={formik.handleChange}/>
+                <p style={{color:'red'}}>{formik.errors.found}</p>
             </label>
+            {formik.errors['lost-or-found'] && (
+                <p style={{ color: 'red' }}>{formik.errors['lost-or-found']}</p>
+            )}
             <button type='submit'>Submit</button>
         </form>
     );
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,14 +1,20 @@
 import {React} from "react";
 import { useFormik } from "formik";
+import * as yup from "yup";
 
 function Signup() {
+
+    const formSchema = yup.object().shape({
+        username: yup.string().required('Username required').max(20),
+        password: yup.string().required('Password required').max(20)
+    });
 
     const formik = useFormik({
         initialValues: {
         username: '',
         password: '',
         },
-        // validate,
+        validationSchema:formSchema,
         onSubmit: (values) => {
             fetch('/signup', {
                 method: 'POST',
@@ -20,19 +26,18 @@ function Signup() {
         },
     })
 
-    // const [username, setUsername] = useState('')
-    // const [password, setPassword] = useState('')
-
 
     return (
         <form onSubmit = {formik.handleSubmit}>
             <label>Username
                 <input type='text' id='username' name='username' value={formik.values.username}
                     onChange={formik.handleChange}/>
+                <p style={{color:'red'}}>{formik.errors.username}</p>
             </label>
             <label>Password
                 <input type='text' id='password' name='password' value={formik.values.password}
                     onChange={formik.handleChange}/>
+                <p style={{color:'red'}}>{formik.errors.password}</p>
             </label>
             <button type='submit'>Submit</button>
         </form>
